b66785b619b1a8ac0bfd5cb071ee630f2055c2b4
Ze-Zheng Wu <zezhengwu@proton.me>
2025-01-27 13:15:30 +0800

    chore: update tests

--- tests/polyfill.spec.ts
+++ tests/polyfill.spec.ts
@@ -11,43 +11,37 @@ describe("Polyfill doesn't take effect on existing non-nullable implementations"
   test("existing string type implementation", async () => {
     const placeholder = "";
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
   test("existing number type implementation", async () => {
     const placeholder = 0;
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
   test("existing boolean type implementation", async () => {
     const placeholder = false;
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
   test("existing document.all type implementation", async () => {
     const placeholder = document.all;
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
   test("existing object type implementation", async () => {
     const placeholder = Object.create(null);
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
   test("existing function type implementation", async () => {
     const placeholder = () => {};
     globalThis.BarcodeDetector = placeholder;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.isTrue(Object.is(globalThis.BarcodeDetector, placeholder));
   });
 });
@@ -63,20 +57,17 @@ describe("Polyfill takes effect on nullable implementations", () => {
   test("existing implementation is deleted", async () => {
     // biome-ignore lint/performance/noDelete: for testing
     delete globalThis.BarcodeDetector;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.instanceOf(globalThis.BarcodeDetector, Function);
   });
   test("existing implementation is undefined", async () => {
     globalThis.BarcodeDetector = undefined;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.instanceOf(globalThis.BarcodeDetector, Function);
   });
   test("existing implementation is null", async () => {
     globalThis.BarcodeDetector = null;
-    const modulePath = `../src/polyfill?update=${Date.now()}}`;
-    await import(modulePath);
+    await import(/* @vite-ignore */ `../src/polyfill?update=${Date.now()}`);
     assert.instanceOf(globalThis.BarcodeDetector, Function);
   });
 });
