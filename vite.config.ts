38d23443fcb29b4b93a48f798fcd47130a5b8089
Ze-Zheng Wu <zezhengwu@proton.me>
2025-05-01 23:50:44 +0800

    chore: enable dangerouslyIgnoreUnhandledErrors in test configuration

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   },
   test: {
     includeTaskLocation: true,
+    dangerouslyIgnoreUnhandledErrors: true,
     browser: {
       enabled: true,
       headless: true,
